138122c2f18a51c70b1c2075309cc641f038a3c3
Brooke Bryan <brooke@justdevelop.it>
2013-02-01 08:49:43 +0000

    rejig

---
+++ src/CubexIo/Applications/Www/Controllers/BaseController.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * @author brooke.bryan
+ * @author gareth.evans
+ */
+
+namespace CubexIo\Applications\Www\Controllers;
+
+use Cubex\Core\Controllers\WebpageController;
+use Cubex\View\Templates\Errors\Error404;
+use CubexIo\Applications\Www\Views\Cubes\Web;
+use CubexIo\Applications\Www\Views\Index;
+use CubexIo\Applications\Www\Views\Section\CubesNav;
+use CubexIo\Applications\Www\Views\Section\MainNav;
+
+abstract class BaseController extends WebpageController
+{
+
+  public function preProcess()
+  {
+    $this->requireCss(
+      "//netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/css/bootstrap-combine" .
+      "d.min.css"
+    );
+    $this->requireCss(
+      "http://fonts.googleapis.com/css?family=Open+Sans:400,600,700"
+    );
+    $this->requireCss("/base");
+    $this->webpage()->addHeaderElement(
+      "<!--[if IE]><script src=\"http://html5shiv.googlecode.com/svn/trunk/htm" .
+      "l5.js\"></script><![endif]-->"
+    );
+  }
+
+  public function preRender()
+  {
+    $this->nest("mainNav", new MainNav($this->getRouteResult()));
+    $this->nest(
+      "cubesNav",
+      new CubesNav($this->request(), $this->getRouteResult())
+    );
+  }
+
+  public function renderNotFound()
+  {
+    $this->webpage()->setStatusCode("404");
+    return new Error404();
+  }
+
+  public function defaultAction()
+  {
+    return "NotFound";
+  }
+}
--- src/CubexIo/Applications/Www/Controllers/DefaultController.php
+++ src/CubexIo/Applications/Www/Controllers/DefaultController.php
@@ -1,87 +1,17 @@
 <?php
 /**
- * @author brooke.bryan
- * @author gareth.evans
+ * @author  brooke.bryan
  */
 
 namespace CubexIo\Applications\Www\Controllers;
 
-use Cubex\Core\Controllers\WebpageController;
-use Cubex\View\Templates\Errors\Error404;
-use CubexIo\Applications\Www\Views\Cubes\Web;
 use CubexIo\Applications\Www\Views\Index;
-use CubexIo\Applications\Www\Views\Section\CubesNav;
-use CubexIo\Applications\Www\Views\Section\MainNav;
 
-class DefaultController extends WebpageController
+class DefaultController extends BaseController
 {
-
-  public function preProcess()
-  {
-    $this->requireCss(
-      "//netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/css/bootstrap-combine".
-      "d.min.css"
-    );
-    $this->requireCss(
-      "http://fonts.googleapis.com/css?family=Open+Sans:400,600,700"
-    );
-    $this->requireCss("/base");
-    $this->webpage()->addHeaderElement(
-      "<!--[if IE]><script src=\"http://html5shiv.googlecode.com/svn/trunk/htm".
-      "l5.js\"></script><![endif]-->"
-    );
-  }
-
-  public function preRender()
-  {
-    $this->nest("mainNav", new MainNav($this->getRouteResult()));
-    $this->nest(
-      "cubesNav",
-      new CubesNav($this->request(), $this->getRouteResult())
-    );
-  }
-
   public function renderIndex()
   {
     return new Index();
-  }
-
-  public function renderAbout()
-  {
-    return null;
-  }
-
-  public function renderLearn()
-  {
-    return null;
-  }
-
-  public function renderDocs()
-  {
-    return null;
-  }
-
-  public function renderCommunity()
-  {
-    return null;
-  }
-
-  public function renderCubes($cube)
-  {
-    switch($cube)
-    {
-      case 'web':
-        return new Web();
-    }
-
-    return $this->renderNotFound();
-  }
-
-  public function renderNotFound()
-  {
-    $this->webpage()->setStatusCode("404");
-
-    return new Error404();
   }
 
   public function getRoutes()
@@ -91,13 +21,7 @@ class DefaultController extends WebpageController
       "/about"           => "about",
       "/learn"           => "learn",
       "/docs"            => "docs",
-      "/community"       => "community",
-      "/cubes/:cube@all" => "cubes"
+      "/community"       => "community"
     );
-  }
-
-  public function defaultAction()
-  {
-    return "notFound";
   }
 }
